0f9dbaf39d6cb292bb7fa0a7189d44fcb6743308
Nop <noppharut@Nops-MacBook-Pro.local>
2026-03-16 10:32:36 +0700

    Host

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bangkokhealthzone',
   server: {
-    allowedHosts: ["extroversive-bao-fiberless.ngrok-free.dev"]
+    allowedHosts: true,
+    host: true
   }
   
 })
